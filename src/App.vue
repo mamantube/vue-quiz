836235bab8d42fd7ede7208001e1d124abcb0238
mamantube <firmanhiday.id007@gmail.com>
2026-05-05 14:35:30 +0700

    create header and card quiz

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,75 @@
 <template>
     <main>
       <header>
-        <h1 id="title">Hello Word</h1>
+        <h1 id="title">Quiz</h1>
+        <input type="text" id="search-input">
       </header>
+      <section id="quiz-container">
+        <div class="card">
+          <img src="https://images.unsplash.com/photo-1516116216624-53e697fedbea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww" alt="">
+          <div class="card-body">
+            <h2>Programming</h2>
+            <p>2 Questions</p>
+          </div>
+        </div>
+      </section>
     </main>
 </template>
+
+<style scoped>
+
+main {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+header {
+  margin-top: 30px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+#title {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+#search-input {
+  border: none;
+  background-color: #c9c9c9a9;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+#quiz-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+}
+
+.card {
+  width: 270px;
+  margin-right: 30px;
+  margin-bottom: 30px;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+
+.card img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  margin: 0;
+}
+
+.card-body {
+  padding: 0 15px;
+}
+
+.card-body h2 {
+  font-weight: bold;
+}
+
+</style>
