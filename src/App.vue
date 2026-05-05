14a5d091cbc4e6a5d48510e8aca239a6b696608f
mamantube <firmanhiday.id007@gmail.com>
2026-05-05 16:32:52 +0700

    create quiz card component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, watch } from "vue";
 import srcQuiz from "./data/quizes.json";
+import QuizCard from "./components/QuizCard.vue";
 
 const quizes = ref(srcQuiz);
 const search = ref("");
@@ -21,13 +22,14 @@ console.log(quizes)
         <input v-model.trim="search" type="text" id="search-input">
       </header>
       <section id="quiz-container">
-        <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
           <img :src="quiz.img" :alt="quiz.title">
           <div class="card-body">
             <h2> {{ quiz.title }}</h2>
             <p> {{ quiz.questions.length }} Questions</p>
           </div>
-        </div>
+        </div> -->
+        <QuizCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
       </section>
     </main>
 </template>
@@ -64,28 +66,6 @@ header {
   margin-top: 20px;
 }
 
-.card {
-  width: 270px;
-  margin-right: 30px;
-  margin-bottom: 30px;
-  border-radius: 5px;
-  overflow: hidden;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-}
 
-.card img {
-  width: 100%;
-  height: 200px;
-  object-fit: cover;
-  margin: 0;
-}
-
-.card-body {
-  padding: 0 15px;
-}
-
-.card-body h2 {
-  font-weight: bold;
-}
 
 </style>
